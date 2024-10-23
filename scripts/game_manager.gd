12adcf7263e228c546121954a60d8d0804c24878
nanobyte1011 <87462432+nanobyte1011@users.noreply.github.com>
2024-10-24 10:29:42 +1100

    Started multiplayer support

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,3 +7,16 @@ var score = 0
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+
+
+func _on_hosting() -> void:
+	%MultiplayerHud.hide()
+	print("Hosting")
+	MultiplayerManager.become_host()
+
+
+func _on_joining() -> void:
+	%MultiplayerHud.hide()
+	print("Joining")
+	MultiplayerManager.join_as_player_2()
+	
---
+++ scripts/multiplayer/multiplayer_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+
+const SERVERPORT = 25565
+const SERVERIP = "127.0.0.1"
+
+# Called when the node enters the scene tree for the first time.
+func become_host() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func join_as_player_2() -> void:
+	pass
